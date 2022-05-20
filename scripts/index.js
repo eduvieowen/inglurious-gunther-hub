096081ebfe0622a41a1c02f5bb8750a5ee5e835c
EduvieOU <eduvieouu@gmail.com>
2022-05-20 12:26:27 +0100

    Added More Dark Mode Features

--- scripts/index.js
+++ scripts/index.js
@@ -9,3 +9,57 @@ burger.addEventListener('click', () => {
         menu.classList.add('hidden');
     }
 })
+
+function darkModeButton() {
+    if (document.getElementById("darkMode").classList.contains("hidden")) 
+    {
+        darkModeFormat();
+
+        document.getElementById("lightMode").classList.add("hidden");
+        document.getElementById("darkMode").classList.remove("hidden");
+
+        document.getElementById("switchButton").classList.remove("bg-sky-500", "border-black", "shadow-round-b-2");
+        document.getElementById("switchButton").classList.add("bg-slate-800", "border-white", "shadow-round-w-2");
+        
+        darkModeFormat();
+    }
+    else 
+    {
+        document.getElementById("darkMode").classList.add("hidden");
+        document.getElementById("lightMode").classList.remove("hidden");
+
+        document.getElementById("switchButton").classList.remove("bg-slate-800", "border-white", "shadow-round-w-2");
+        document.getElementById("switchButton").classList.add("bg-sky-500", "border-black", "shadow-round-b-2");
+
+    }
+
+}
+// darkModeFormat();
+
+function darkModeFormat() {
+    if (document.getElementById("mainSection").classList.contains("bg-gray-200"))
+    {
+        document.getElementById("navMenu").classList.remove("bg-white");
+        document.getElementById("navMenu").classList.add("bg-gray-800");
+        
+        document.getElementById("menuOne").classList.add("card-3");
+        document.getElementById("menuTwo").classList.add("card-3");
+        document.getElementById("menuThree").classList.add("card-3");
+        
+        document.getElementById("mainSection").classList.remove("bg-gray-200");
+        document.getElementById("mainSection").classList.add("bg-gray-800");
+        
+        document.getElementById("mainTitle").classList.add("text-green-400");
+        document.getElementById("mainSubTitle").classList.add("text-white");
+        
+        document.querySelector("h4").classList.add("text-white", "border-white");
+        document.getElementById("headFour").classList.add("text-white", "border-white");
+
+    }
+    else
+    {
+        document.getElementById("mainSection").classList.remove("bg-gray-800");
+        document.getElementById("mainSection").classList.add("bg-gray-200");
+
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,16 @@ module.exports = {
 
   theme: {
     extend: {
+
       fontFamily: {
         body: ['Nunito']
+      },
+
+      boxShadow: {
+        'round-b-2': '0px 0px 5px 2px rgb(0,0,0)',
+        'round-w-2': '0px 0px 5px 2px rgb(255,255,255)',
       }
+      
     },
   },
   
